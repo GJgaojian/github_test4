91bb7d3b832da0d4b885eb76486600a485ddc6dc
2101class <13141234762@163.com>
2021-08-24 14:48:32 +0800

    小黑:v3.0

--- src/main/java/cn/baizhi/A.java
+++ src/main/java/cn/baizhi/A.java
@@ -3,6 +3,11 @@ package cn.baizhi;
 //小明
 public class A {
     public static void main(String[] args) {
+
+        int f = 22;
+        int t = 33;
+
+
         int a = 10;
         System.out.println(a);
 
